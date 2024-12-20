839629bd61ba4d7cf4636988ac03671e5900d328
DavidBenoit-TR <mdavid.benoit@gmail.com>
2024-12-20 08:35:37 -0600

    Ojetos DTO

--- obj/Debug/net8.0/WingsAir_API.AssemblyInfo.cs
+++ obj/Debug/net8.0/WingsAir_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WingsAir_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e74999e984a7249476b6ebf584fb586bdeb3b958")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1593e44ab2bcdbb78ce7d73021e2f09f411d59c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WingsAir_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WingsAir_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
