4ef562853d8a665226c38f30c782c575bfa74a57
DavidBenoit-TR <mdavid.benoit@gmail.com>
2024-12-20 09:25:19 -0600

    Configuración de Program.cs + Cadena de Conexión appsetting.json + Autegenerate false

--- Program.cs
+++ Program.cs
@@ -1,11 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using WingsAir_API.Dependency;
+using WingsAir_API.Models;
+using WingsAir_API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+//inyecto el contexto creado con EF al constructor de la aplicación para que se conecte
+builder.Services.AddDbContext<Aeropuerto_WingsAirContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("sql_connection_remote"), sqlServerOptionsAction: sqloption =>
+{
+    sqloption.EnableRetryOnFailure(
+        maxRetryCount: 10,
+        maxRetryDelay: TimeSpan.FromSeconds(10),
+        errorNumbersToAdd: null);
+}));
+//Inyección de dependecias (Scoped<Intefaz, Servicio>)
+builder.Services.AddScoped<IVuelos, VuelosService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//CORS
+builder.Services.AddCors(option => option.AddPolicy("AllowAnyOrigin", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 
 var app = builder.Build();
 
@@ -16,6 +35,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAnyOrigin");
 app.UseAuthorization();
 
 app.MapControllers();
